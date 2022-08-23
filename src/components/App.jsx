428c91c3043c1363a6b4415c166c5bee399fa262
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-23 22:01:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import Searchbar from './Searchbar/Searchbar';
+
 export const App = () => {
+  state = {
+    smallImages: [],
+    largeImageURL: '',
+  };
   return <div>React homework template</div>;
 };
