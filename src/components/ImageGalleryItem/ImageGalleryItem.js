4d10207db5b5ed6de75b035d55e01c13c4a7f8bb
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-25 19:30:07 +0300

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,3 +1,28 @@
-<li class="gallery-item">
+import PropTypes from 'prop-types';
+import css from './ImageGalleryItem.module.css';
+
+function ImageGalleryItem({ webformatURL, largeImageURL, tags, onOpenModal }) {
+  return (
+    <li className={css.item}>
+      <img
+        src={webformatURL}
+        alt={tags}
+        data-source={largeImageURL}
+        className={css.image}
+        onClick={onOpenModal}
+      />
+    </li>
+  );
+}
+
+ImageGalleryItem.propTypes = {
+  largeImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+};
+
+export default ImageGalleryItem;
+
+/* <li class="gallery-item">
   <img src="" alt="" />
-</li>;
+</li>; */
