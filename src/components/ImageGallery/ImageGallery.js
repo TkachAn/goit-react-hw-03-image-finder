7b199ddf915bba62003fdc329ef81e0b08db5334
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-25 19:30:32 +0300

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,3 +1,30 @@
-<ul class="gallery">
+import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import css from './ImageGallery.module.css';
+
+function ImageGallery({ images, onOpenModal }) {
+  return (
+    <ul className={css.list}>
+      {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          tags={tags}
+          onOpenModal={onOpenModal}
+        />
+      ))}
+    </ul>
+  );
+}
+
+ImageGallery.propTypes = {
+  images: PropTypes.array.isRequired,
+  onOpenModal: PropTypes.func.isRequired,
+};
+
+export default ImageGallery;
+
+/* <ul class="gallery">
   <li> с изображениями </li>
-</ul>;
+</ul>; */
