2349edf85f6dae8d105ecce9b67b5566a89e32d5
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-25 18:58:55 +0300

    Create apiPixabay.js

---
+++ src/apiPixabay/apiPixabay.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '28325573-e3f151920507aabfaddea723c';
+
+const apiPixabay = async (query, page) => {
+  const { data } = await axios.get('', {
+    params: {
+      key: KEY,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page: 12,
+      q: query,
+      page: page,
+    },
+  });
+  return data.hits;
+};
+
+export default apiPixabay;
