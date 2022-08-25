97fc7eaff4e04055f7258e7e175a60e46bb5edac
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-25 19:31:26 +0300

    Update Loader.js

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,19 @@
+// import Loader from 'react-loader-spinner';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+// import css from './Loader.module.css';
+
+// function Loader_() {
+//   return (
+//     <div className={css.overlay}>
+//       <Loader
+//         type="ThreeDots"
+//         color="#00BFFF"
+//         height={100}
+//         width={100}
+//         timeout={0}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Loader_;
