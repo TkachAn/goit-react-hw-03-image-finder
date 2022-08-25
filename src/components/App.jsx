18c65f2c8eabe39379280621de5e258199d96e49
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-25 19:31:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,141 @@
+import { Component } from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import apiPixabay from '../apiPixabay/apiPixabay';
+// import Container from './components/Container';
 import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+// import Loader_ from './Loader/Loader';
+import Modal from './Modal/Modal';
+import ErrorView from './ErrorView/ErrorView';
 
-export const App = () => {
+class App extends Component {
   state = {
+    query: '',
     images: [],
     largeImageURL: '',
-    query: '',
+    page: 1,
+    error: null,
+    isLoading: false,
+    showModal: false,
   };
-  return <div>React homework template</div>;
-};
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.setState({ images: [], page: 1, error: null });
+    }
+  }
+
+  searchImages = async () => {
+    const { query, page } = this.state;
+
+    if (query.trim() === '') {
+      return; //toast.info('Please enter a value for search images!');
+    }
+
+    // this.toggleLoader();
+
+    try {
+      const request = await apiPixabay(query, page);
+      this.setState(({ images, page }) => ({
+        images: [...images, ...request],
+        page: page + 1,
+      }));
+      if (request.length === 0) {
+        this.setState({ error: `No results were found for ${query}!` });
+      }
+    } catch (error) {
+      this.setState({ error: 'Something went wrong. Try again.' });
+    } finally {
+      <p>qq</p>; // this.toggleLoader();
+    }
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.searchImages();
+  };
+
+  onLoadMore = () => {
+    this.searchImages();
+    this.scrollPage();
+  };
+
+  onOpenModal = e => {
+    this.setState({ largeImageURL: e.target.dataset.source });
+    this.toggleModal();
+  };
+
+  toggleLoader = () => {
+    this.setState(({ isLoading }) => ({
+      isLoading: !isLoading,
+    }));
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  scrollPage = () => {
+    setTimeout(() => {
+      window.scrollBy({
+        top: document.documentElement.clientHeight - 160,
+        behavior: 'smooth',
+      });
+    }, 1000);
+  };
+
+  render() {
+    const { query, images, largeImageURL, isLoading, showModal, error } =
+      this.state;
+    return (
+      <div>
+        <Searchbar
+          onHandleSubmit={this.handleSubmit}
+          onSearchQueryChange={this.handleChange}
+          value={query}
+        />
+
+        {error && <ErrorView texterror={error} />}
+
+        {images.length > 0 && !error && (
+          <ImageGallery images={images} onOpenModal={this.onOpenModal} />
+        )}
+
+        {isLoading && <p>загрузка...</p>}
+
+        {!isLoading && images.length >= 12 && !error && (
+          <Button onLoadMore={this.onLoadMore} />
+        )}
+
+        {showModal && (
+          <Modal
+            onToggleModal={this.toggleModal}
+            largeImageURL={largeImageURL}
+          />
+        )}
+        {!query && <p>введите запрос!</p>}
+      </div>
+    );
+  }
+}
+
+export default App;
+//ToastContainer autoClose={3700}
+//<Loader_ />}
+
+// export const App = () => {
+//   state = {
+//     images: [],
+//     largeImageURL: '',
+//     query: '',
+//   };
+//   return <div>React homework template</div>;
+// };
