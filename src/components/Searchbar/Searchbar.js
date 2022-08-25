53acd638fa74f481c6e05957d7cc598ade59f66e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-25 19:25:27 +0300

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,24 +1,32 @@
+import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
 function Searchbar({ onHandleSubmit, onSearchQueryChange, value }) {
   return (
-    <header className={css.searchbar}>
-      <form className={css.form}>
-        <button type="submit" className={css.button} onClick={onHandleSubmit}>
-          <span className="button-label">Search</span>
+    <header className={css.header}>
+      <form className={css.form} onSubmit={onHandleSubmit}>
+        <button type="submit" className={css.btn}>
+          <span className={css.label}>Search</span>
         </button>
+
         <input
           className={css.input}
           type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
           value={value}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
           onChange={onSearchQueryChange}
         />
       </form>
     </header>
   );
 }
+
+Searchbar.propTypes = {
+  onHandleSubmit: PropTypes.func.isRequired,
+  onSearchQueryChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default Searchbar;
