6e4a0c55b14dd12013f71451c079722c639a7b32
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-26 15:46:59 +0300

    Create preLoad.js

---
+++ src/components/preLoad/preLoad.js
@@ -0,0 +1,14 @@
+// import { apiIdPixabay } from '../../apiPixabay/apiPixabay';
+function PreLoad() {
+  // const { id } = this.state;
+  // const largeImageURL = apiIdPixabay.get(id);
+
+  return (
+    <div className="gg">
+      <img src="../img/finger.jpg" alt="hh" />
+      <h2>'Please enter a value for search images!'</h2>
+    </div>
+  );
+}
+export default PreLoad;
+//{css.modal}>
