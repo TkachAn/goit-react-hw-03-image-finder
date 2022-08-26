6cc1b0c858e59e8dd555812d383d369ed1f1e925
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-26 15:47:15 +0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,19 +1,21 @@
-// import Loader from 'react-loader-spinner';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-// import css from './Loader.module.css';
+import { ThreeDots } from 'react-loader-spinner'; ///dist/loader/ThreeDots
+import css from './Loader.module.css';
 
-// function Loader_() {
-//   return (
-//     <div className={css.overlay}>
-//       <Loader
-//         type="ThreeDots"
-//         color="#00BFFF"
-//         height={100}
-//         width={100}
-//         timeout={0}
-//       />
-//     </div>
-//   );
-// }
+function Loader() {
+  return (
+    <div className={css.overlay}>
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#4fa94d"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </div>
+  );
+}
 
-// export default Loader_;
+export default Loader;
