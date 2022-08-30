b6281fa5a1b77603b2441c82b85e412e58643771
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-31 00:51:55 +0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,17 +3,19 @@ import css from './Loader.module.css';
 
 function Loader() {
   return (
-    <div className={css.overlay}>
-      <ThreeDots
-        height="100"
-        width="100"
-        radius="9"
-        color="#4fa94d"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClassName={css.wrap}
-        visible={true}
-      />
+    <div className={css.div}>
+      <div className={css.overlay}>
+        <ThreeDots
+          height="100"
+          width="100"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName={css.wrap}
+          visible={true}
+        />
+      </div>
     </div>
   );
 }
